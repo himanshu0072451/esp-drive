795d5ac0b3e036d1fea480bfeda28fa4921e32f1
Himanshu <himanshuingle42@gmail.com>
2025-02-07 06:22:18 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/DriftX/', // Replace with your repo name
   plugins: [react()],
-  base: '/DriftX/' // Replace with your repo name
 });
