bc23a52cfd045b9734164d936a83e8c764d88449
Himanshu Ingle <134830474+himanshu0072451@users.noreply.github.com>
2025-02-07 06:45:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/DriftX/', // Replace with your repo name
+  base: '/esp-drive/', // Replace with your repo name
   plugins: [react()],
+server: {
+    proxy: {
+      '/control': {
+        target: 'http://192.168.147.74', // Your ESP8266 IP
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
